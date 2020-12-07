db7a2f3a508bff356bde2e956c80f841cd55b92e
Aaron Romero <046aaronromero@gmail.com>
2020-12-06 19:39:06 -0800

    Declaring some types in go

--- tutorial.go
+++ tutorial.go
@@ -5,4 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Learning Go, whooohooooo!")
 	fmt.Println("The most complex Golang program ever created!")
+
+	//Declaring Data Types
+
+	var name string = "Skxter"
+	var number uint16 = 260
+
+	number = number + 10
+
+	fmt.Println("Sum", number)
+	fmt.Println("My name is", name)
 }
