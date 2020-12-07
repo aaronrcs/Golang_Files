56fa51be09ee8eeb58a1247e9777d5f531f3b548
Aaron Romero <046aaronromero@gmail.com>
2020-12-06 19:46:47 -0800

    Showing a different way of declaring a variable in Go, coooool

--- tutorial.go
+++ tutorial.go
@@ -15,4 +15,10 @@ func main() {
 
 	fmt.Println("Sum", number)
 	fmt.Println("My name is", name)
+
+	// Another way of declaring a variable
+
+	age := 25 //Go guessing the type of value being assigned to variable
+
+	fmt.Println("I am", age, "years old.")
 }
