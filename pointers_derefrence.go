5bcdfe7e395bb7ab80db794650373716fe9c53fa
Aaron Romero <046aaronromero@gmail.com>
2020-12-13 20:32:06 -0800

    Basics of pointers, now to use a function for as an example

--- pointers_derefrence.go
+++ pointers_derefrence.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+	// & and * Operators
+
+	// & --> Pointer or reference of the memeory location where value is stored in computer
+	// * --> Derefrence or access the value in which its pointing to
+
+	x := 7
+	y := &x
+
+	fmt.Println("X: ", x, " Y: ", y)
+
+	// Using a '*' to derefrence from the pointer to modify the value of y
+	*y = 8
+
+	fmt.Println("X: ", x, " Y: ", y)
 
 }
